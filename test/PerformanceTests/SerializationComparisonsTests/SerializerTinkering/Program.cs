447f2b35537634deebeed88dbc73de6283618345
Clément Habinshuti <clhabins@microsoft.com>
2024-01-25 12:07:23 +0300

    Test basic custom JsonConverter for OData

---
+++ test/PerformanceTests/SerializationComparisonsTests/SerializerTinkering/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExperimentsLib;
+
+Console.WriteLine("Hello, World!");
+
+var writers = DefaultWriterCollection.Create();
+var writer = writers.GetWriter("NewWriter");
+var data = CustomerDataSet.GetCustomers(2);
+
+var stream = new MemoryStream();
+
+await writer.WritePayloadAsync(data, stream);
+
+stream.Position = 0;
+StreamReader reader = new StreamReader(stream);
+var content = reader.ReadToEnd();
+Console.WriteLine(content);
